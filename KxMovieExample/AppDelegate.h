da040a1556fba6de7652d4912c05857261c4e833
Kolyvan <ru.kolyvan@gmail.com>
2012-10-25 21:53:21 +1100

    added license notice

--- KxMovieExample/AppDelegate.h
+++ KxMovieExample/AppDelegate.h
@@ -5,6 +5,9 @@
 //  Created by Kolyvan on 11.10.12.
 //  Copyright (c) 2012 Konstantin Boukreev . All rights reserved.
 //
+//  https://github.com/kolyvan/kxmovie
+//  this file is part of KxMovie
+//  KxMovie is licenced under the LGPL v3, see lgpl-3.0.txt
 
 #import <UIKit/UIKit.h>
 
--- kxmovie/KxAudioManager.m
+++ kxmovie/KxAudioManager.m
@@ -5,6 +5,9 @@
 //  Created by Kolyvan on 23.10.12.
 //  Copyright (c) 2012 Konstantin Boukreev . All rights reserved.
 //
+//  https://github.com/kolyvan/kxmovie
+//  this file is part of KxMovie
+//  KxMovie is licenced under the LGPL v3, see lgpl-3.0.txt
 
 // ios-only and output-only version of Novocaine https://github.com/alexbw/novocaine
 // Copyright (c) 2012 Alex Wiltschko
